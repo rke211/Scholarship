7b9881196170d580a2825698fd9eb6961be7405b
Ryan Kelly <67314686+rke211@users.noreply.github.com>
2021-08-26 21:57:58 +0100

    fix variable spelling

--- classes/database.php
+++ classes/database.php
@@ -3,7 +3,7 @@ if ( !defined( 'allowAccess' ) )exit( 'No direct script access allowed' );
 
 include_once( __DIR__ . "/../config.php" );
 
-
+class databaseConnection extends Exception { }
 
 class database
 
@@ -13,13 +13,13 @@ class database
 
   private static $_instance; //The single instance
 	
-  private $_host = DB_HOST;
+  private $_host = '';
 
-  private $_username = DB_USER;
+  private $_username = '';
 
-  private $_password = DB_PASSWORD;
+  private $_password = '';
 
-  private $_database = DB_NAME;
+  private $_database = '';
 
   /*
 
@@ -54,7 +54,7 @@ class database
   private function __construct() {
 	  $config =parse_ini_file(__DIR__ . "/../config.ini");
 	  $this->_host = $config['db_host'];
-	  $this->_databse = $config['db'];
+	  $this->_database = $config['db'];
 	  $this->_username = $config['db_username'];
 	  $this->_password = $config['db_password'];
 
@@ -63,7 +63,7 @@ class database
       $this->_connection = new PDO( "mysql:host=$this->_host;dbname=" . $this->_database, $this->_username, $this->_password );
       $this->_connection->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
     } catch ( PDOException $e ) {
-      trigger_error( "Connection failed: " . $e->getMessage(), E_USER_ERROR );
+      throw new databaseConnection( "Connection failed: " . $e->getMessage(), E_USER_ERROR );
     }
 
 
